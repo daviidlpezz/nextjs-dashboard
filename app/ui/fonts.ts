e1c8a5ce83bcac2b25e19c2ef0c0f94c2280ac48
daviidlpezz <dosittydanete@gmail.com>
2025-04-22 23:21:38 +0200

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const font = Inter({ subsets: ['latin'], });
+
+export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700'], });
